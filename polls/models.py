2faf96c118243a95a60ebd7dd47f45f237000702
nishakathir97 <nishakathir97@gmail.com>
2025-06-27 08:43:10 +0530

    add details ,index,results also testing

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from datetime import datetime, date, timedelta
 from django.utils import timezone
 
+
+
 # Create your models here.
 
 class Blog(models.Model):
@@ -25,12 +27,13 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')  
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - timedelta(days=1)
+        now = timezone.now()
+        return now - timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
-
-
+   
+        
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -53,3 +56,5 @@ class Entry(models.Model):
 
     def __str__(self):
         return self.headline
+
+    
